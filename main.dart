8c7ce4bda5bd9fc8aaa8afeb880b6ec44d1b0ee0
SimranKTECH <71891173+SimranKTECH@users.noreply.github.com>
2022-08-13 17:42:26 +0530

    routes desc

---
+++ main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+import './login.dart';
+import 'register.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'login',
+      routes: {
+        'login': (context) => MyLogin(),
+        'register': (context) => MyRegister(),
+        'home': (context) => MyHomePage(),
+      },
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //drawer: NavigationBar(),
+      appBar: AppBar(),
+    );
+  }
+}
